067ca1a5eb08500199d98f96deebd1076b87afc7
Maciej Cichocki <maciek.ce3@gmail.com>
2024-10-15 16:29:29 +0200

    Add project files.

---
+++ MemoryKonsola/Card.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryKonsola
+{
+	internal class Card
+	{
+		public string HiddenText;
+		public bool IsHidden;
+		private static readonly int width = 6;
+		private static readonly int height = 6;
+		public Card(string Text) { HiddenText = Text; }
+		public void Draw()
+		{
+			if (!IsHidden)
+			{
+				DrawShown();
+				return;
+			}
+			DrawShown();
+		}
+		public void DrawShown()
+		{
+
+		}
+		public void DrawHidden()
+		{
+
+		}
+	}
+}
---
+++ MemoryKonsola/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryKonsola
+{
+	public class Game
+	{
+		private int width;
+		private int height;
+		private 
+	}
+}
